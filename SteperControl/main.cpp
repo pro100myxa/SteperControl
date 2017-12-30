6ac84acac55e5addd327eefe7b1a60518de9a5fc
Vitaliy <33747111+pro100myxa@users.noreply.github.com>
2017-12-30 21:23:27 +0200

    settings

--- SteperControl/main.cpp
+++ SteperControl/main.cpp
@@ -24,40 +24,9 @@ bool SX_END, SY_END, SZ_END;
 #define SJ_STEP         3       //  		15 
 #define SJ_DIR          2       //  		13
 
-void AccelStepperSetup() {
-	AccelStepper SX (SX_DIR, SX_STEP); //using 5 and 4 pins for direction and step, 1 - "external driver" mode (A4988) // Stepper X asix
 
-	SX.setMaxSpeed(1000); // set max speed of stepper motor (When driving, the stepper motor will accelerate to this maximum speed and slow down when approaching the end of the movement.)
+	
 
-	SX.setAcceleration(100); //Set the acceleration, in steps per second
-
-	SX.setSpeed(100); //Set the speed in steps per second. The movement itself is started by runSpeed ().
-
-
-	AccelStepper SY (SX_DIR, SX_STEP); //Stepper Y asix
-
-	SY.setMaxSpeed(1000);
-
-	SY.setAcceleration(100);
-
-	SY.setSpeed(100);
-
-
-	AccelStepper SZ (SX_DIR, SX_STEP); //Stepper Z asix
-
-	SZ.setMaxSpeed(1000);
-
-	SZ.setAcceleration(100);
-
-	SZ.setSpeed(100);
-
-
-	AccelStepper SJ ( SX_DIR, SX_STEP); //Stepper jaws
-
-	SJ.setMaxSpeed(1000);
-
-	SJ.setSpeed(100);
-}
 
 int main(void)
 {
@@ -66,13 +35,61 @@ int main(void)
 	pinMode(SX_STEP, OUTPUT);       	pinMode(SY_STEP, OUTPUT);       	pinMode(SZ_STEP, OUTPUT); 		pinMode(SJ_STEP, OUTPUT);
 	pinMode(SX_DIR, OUTPUT);		 	pinMode(SY_DIR, OUTPUT);		 	pinMode(SZ_DIR, OUTPUT); 		pinMode(SJ_DIR, OUTPUT);
 	pinMode(SX_END, INPUT); 			pinMode(SY_END, INPUT); 			pinMode(SZ_END, INPUT);
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+	AccelStepper SX(SX_DIR, SX_STEP); //using 5 and 4 pins for direction and step, 1 - "external driver" mode (A4988) // Stepper X asix
 
-	AccelStepperSetup();
+	SX.setMaxSpeed(1000); // set max speed of stepper motor (When driving, the stepper motor will accelerate to this maximum speed and slow down when approaching the end of the movement.)
 
+	SX.setAcceleration(100); //Set the acceleration, in steps per second
+
+	SX.setSpeed(100); //Set the speed in steps per second. The movement itself is started by runSpeed ().
+
+	//////////////////////////////////////////////////////////////////////////
+	AccelStepper SY(SX_DIR, SX_STEP); //Stepper Y asix
+
+	SY.setMaxSpeed(1000);
+
+	SY.setAcceleration(100);
+
+	SY.setSpeed(100);
+
+	//////////////////////////////////////////////////////////////
+	AccelStepper SZ(SX_DIR, SX_STEP); //Stepper Z asix
+
+	SZ.setMaxSpeed(1000);
+
+	SZ.setAcceleration(100);
+
+	SZ.setSpeed(100);
+
+	//////////////////////////////////////////////////////////////
+	AccelStepper SJ(SX_DIR, SX_STEP); //Stepper jaws
+
+	SJ.setMaxSpeed(1000);
+
+	SJ.setSpeed(100);
+	//////////////////////////////////////////////////////////////////////
+	
+	
+	
+	SX.move(distanceX);
+	SX.run();
+	SX.currentPosition();
+
+	SY.move(distanceY);
+	SY.run();
+	SY.currentPosition();
+
+	SZ.move(distanceZ);
+	SZ.run();
+	SZ.currentPosition();
 	
 
-	//SX.runSpeed();
+	SJ.move(distanceJ);
+	SJ.run();
+	SJ.currentPosition();
+	
 
 
 
